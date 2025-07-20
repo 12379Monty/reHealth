86e4bfba8c501cee2cd3c15150b7127df37c4cfd
12379Monty <francois.collin@gmail.com>
2025-07-20 07:21:36 -0700

    ...

--- Scripts/help_files/loadObj.r
+++
@@ -1,7 +0,0 @@
-function (FileName = "", ObjName = "", DataDir = file.path(WRKDIR, 
-    "Data")) 
-{
-    load(file.path(DataDir, FileName))
-    assign(ObjName, get(FileName), pos = 1)
-    rm(list = FileName)
-}
--- Scripts/help_files/saveObj.r
+++
@@ -1,7 +0,0 @@
-function (FileName = "", ObjName = "", DataDir = file.path(WRKDIR, 
-    "Data")) 
-{
-    assign(FileName, get(ObjName))
-    save(list = FileName, file = file.path(DataDir, FileName))
-    rm(list = FileName)
-}
